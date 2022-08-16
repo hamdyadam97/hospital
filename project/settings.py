709ea7054ee8d75bf041c00377a43c40ffe92388
Hamdy Adam <hamdyadam97@gmail.com>
2022-08-16 21:27:06 +0200

    email

--- project/settings.py
+++ project/settings.py
@@ -147,8 +147,8 @@ CORS_ORIGIN_WHITELIST = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'hamdyadam543@gmail.com'
-EMAIL_HOST_PASSWORD = 'agijiqkskhqkittl'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
 DEFAULT_FROM_EMAIL = 'default from email'
