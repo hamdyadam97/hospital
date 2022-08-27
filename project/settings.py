12d3611a0e955146c5571afb91a40bdffb2b1848
Hamdy Adam <hamdyadam97@gmail.com>
2022-08-28 00:52:22 +0200

    heroku edit787

--- project/settings.py
+++ project/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [BASE_DIR/'static/']
 django_heroku.settings(locals())
 
